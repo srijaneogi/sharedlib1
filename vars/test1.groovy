68c5537d293ee99fb418126ced2c825ea18d86ad
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-08-11 13:32:46 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -4,10 +4,10 @@ def call(name){
         def creds = 'gitnewusethis'
         def sharedlib = 'https://github.com/srijaneogi/sharedlib1.git'
         stage('Preparation') { 
-            dir('thisdir') {
+//             dir('thisdir') {
                 echo "${name} hello from shared lib 1"
                 git credentialsId: creds, url: sharedlib, branch: 'master'
-            }
+//             }
             echo "2222222222 ${name} hello from shared lib 1"
         }
     }
