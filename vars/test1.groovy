e3e2ef95340a45dc70c58fdf213c78e00d46993b
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-08-30 14:05:23 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -10,5 +10,14 @@ def call(name){
 //             }
             echo "2222222222 ${name} hello from shared lib 1"
         }
+        stage('validation') { 
+            echo "${name} hello from shared lib 1 stage 2"
+            if ( no == 1) {
+                error "Need to abort this build"
+            }
+        }
+        stage('conclusion') { 
+            echo "${name} hello from shared lib 1"
+        }
     }
 }
