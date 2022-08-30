9a971ae4ae26365eb80c0e5c50d68222d746f820
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-08-30 14:03:22 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,7 +1,17 @@
 def call(name){
     node {
         def mvnHome
+        def no=1
         stage('Preparation') { 
+            echo "${name} hello from shared lib 1"
+        }
+        stage('validation') { 
+            echo "${name} hello from shared lib 1 stage 2"
+            if ( no == 1) {
+                error "Need to abort this build"
+            }
+        }
+        stage('conclusion') { 
             echo "${name} hello from shared lib 1"
         }
     }
