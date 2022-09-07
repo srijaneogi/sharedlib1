d11a3a02cf2c20d1ddc79cae79b894aaf999ff0d
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-09-07 09:58:20 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -4,12 +4,13 @@ def call(name){
 
     	stages {
         	stage('Hello') {
-			when {
-                        	expression {
-                            		env.BRANCH_NAME == 'dev' || env.BRANCH_NAME == 'master'
-                        	}
-                    	}
+// 			when {
+//                         	expression {
+//                             		env.BRANCH_NAME == 'qat' || env.BRANCH_NAME == 'master'
+//                         	}
+//                     	}
             		steps {
+				echo "env.BRANCH_NAME"
                 		echo 'Hello World'
 				echo "2222222222 ${name} hello from shared lib 1"
             		}
