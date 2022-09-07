e59364f4f9c64a079bc9b806a6e1be364359b334
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-09-07 09:32:25 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,34 +1,36 @@
 def call(name){
     pipeline {
-    agent any
+        agent any
         def mvnHome
         def creds = 'gitnewusethis'
         def sharedlib = 'https://github.com/srijaneogi/sharedlib1.git'
         def no = 1
         try {
-        stage('Preparation') {
-            when {
-                expression {
-                    env.BRANCH_NAME == 'dev' || env.BRANCH_NAME == 'qat'
+            stages {    
+                stage('Preparation') {
+                    when {
+                        expression {
+                            env.BRANCH_NAME == 'dev' || env.BRANCH_NAME == 'qat'
+                    }
                 }
-            }
-//             dir('thisdir') {
-                echo "${name} hello from shared lib 1"
-                git credentialsId: creds, url: sharedlib, branch: 'master'
-//             }
-            echo "2222222222 ${name} hello from shared lib 1"
-        }
-        stage('validation') { 
-            echo "${name} hello from shared lib 1 stage 2"
-            if ( no == 1) {
-                error "Need to abort this build"
-                sleep 5
-            }
-        }
-        stage('conclusion') { 
-            echo "${name} hello from shared lib 1 stage 3"
-        }
-        } catch(e) {
+//                  dir('thisdir') {
+                        echo "${name} hello from shared lib 1"
+                        git credentialsId: creds, url: sharedlib, branch: 'master'
+//                  }
+                    echo "2222222222 ${name} hello from shared lib 1"
+                }
+                stage('validation') { 
+                    echo "${name} hello from shared lib 1 stage 2"
+                    if ( no == 1) {
+                        error "Need to abort this build"
+                        sleep 5
+                    }
+                }
+                stage('conclusion') { 
+                    echo "${name} hello from shared lib 1 stage 3"
+                }
+            }   
+          } catch(e) {
             echo "This will run only if failed"
             currentBuild.result = 'FAILURE'
         
