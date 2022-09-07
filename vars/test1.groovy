9b46d7f30e89cd46de0d5036a932799dda944fec
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-09-07 09:50:53 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,40 +1,15 @@
 def call(name){
     pipeline {
-        agent any
-        def mvnHome
-        def creds = 'gitnewusethis'
-        def sharedlib = 'https://github.com/srijaneogi/sharedlib1.git'
-        def no = 1
-            stages {    
-                stage('Preparation') {
-                    when {
-                        expression {
-                            env.BRANCH_NAME == 'dev' || env.BRANCH_NAME == 'qat'
-                        }
-                    }
-                    
-//                  dir('thisdir') {
-						steps {
-							echo "${name} hello from shared lib 1"
-							git credentialsId: creds, url: sharedlib, branch: 'master'
-							echo "2222222222 ${name} hello from shared lib 1"
-						}
-					}		
-                }
-                stage('validation') { 
-                    steps {
-                        echo "${name} hello from shared lib 1 stage 2"
-                        if ( no == 1) {
-                            error "Need to abort this build"
-                            sleep 5
-                        }
-                    }    
-                }
-                stage('conclusion') { 
-                    steps {
-                        echo "${name} hello from shared lib 1 stage 3"
-                    }
-                }
-            }    
-	}
+    	agent any
+
+    	stages {
+        	stage('Hello') {
+            		steps {
+                		echo 'Hello World'
+				echo "2222222222 ${name} hello from shared lib 1"
+            		}
+        	}
+    	}
+   }
+
 }
