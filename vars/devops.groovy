18ab12a5afdc2d08c516db254000c6df689272a0
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-09-15 19:16:09 +0530

    Update devops.groovy

--- vars/devops.groovy
+++ vars/devops.groovy
@@ -1,1 +1,18 @@
-def map = [name: "Jerry", age: 42, city: "New York"]
+def call(mapname){
+    pipeline {
+    	agent any
+    	stages {
+        	stage('Hello') {
+			
+            	steps {
+					      script {
+						      def b = env.BRANCH_NAME
+						      echo "$b"
+						      echo "2222222222 ${mapname} hello from shared lib 1"
+                  echo "${mapname[name]}"
+					      }
+            	}
+        	}
+    	}
+   }
+}
